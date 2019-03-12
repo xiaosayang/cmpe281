a659728c28c604eed6e63230699f2d7d2b385827
xiaosayang <47075033+xiaosayang@users.noreply.github.com>
2019-03-12 01:17:45 -0700

    Create test_app.py

---
+++ lab2_files/test_app.py
@@ -0,0 +1,12 @@
+import webtest
+
+import main
+
+
+def test_get():
+    app = webtest.TestApp(hello281.app)
+
+    response = app.get('/')
+
+    assert response.status_int == 200
+    assert response.body == 'Hello, welcome to my homepage for CMPE202 lab2!'
